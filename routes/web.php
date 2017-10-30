cf325053fbe05f83572bca0d4f67eb33462f91eb
vhhieu95 <hieupr9695@gmail.com>
2017-10-30 16:17:23 +0700

    show news

--- routes/web.php
+++ routes/web.php
@@ -11,42 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
-    return view('frontend.home.index');
-});
-
-Route::get('/sanpham', function () {
-    return view('frontend.products.index');
-});
-
-Route::get('chitiet-sanpham', function () {
-    return view('frontend.products.show');
-});
-
-Route::get('duan', function () {
-    return view('frontend.projects.index');
-});
-
-Route::get('chitiet-duan', function () {
-    return view('frontend.projects.show');
-});
-Route::get('chitiet-tintuc', function () {
-    return view('frontend.news.show');
-});
-Route::get('tintuc', function () {
-    return view('frontend.news.index');
-});
-Route::get('lienhe', function () {
-    return view('frontend.contact.index');
-});
-
-Route::get('gioithieu', function () {
-    return view('frontend.about-us.index');
-});
-Route::get('/admin', function () {
-    return view('backend.home.index');
-})->name('admin.index');
+Route::resource('/news', 'NewsController');
