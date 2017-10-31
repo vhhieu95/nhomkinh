791b1818b7070452492fef487f3f064980b65222
Duoc Nguyen C <satthukt1232@gmail.com>
2017-10-31 15:24:44 +0700

    Login

--- resources/views/backend/layouts/partials/header.blade.php
+++ resources/views/backend/layouts/partials/header.blade.php
@@ -1,117 +1,36 @@
 <header class="main-header">
-    <a href="{{ route('admin.index') }}" class="logo">
-      <span class="logo-mini"><b>{{ __('Ad') }}</b></span>
-      <span class="logo-lg"><b>{{ __('Admin') }}</b></span>
+  <a href="{{ route('admin.index') }}" class="logo">
+    <span class="logo-mini"><b>{{ __('Ad') }}</b></span>
+    <span class="logo-lg"><b>{{ __('Admin') }}</b></span>
+  </a>
+  <nav class="navbar navbar-static-top">
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
     </a>
-    <nav class="navbar navbar-static-top">
-      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
-      </a>
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <li class="dropdown messages-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-envelope-o"></i>
-              <span class="label label-success">4</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 4 messages</li>
-              <li>
-                <ul class="menu">
-                  <li>
-                    <a href="#">
-                      <div class="pull-left">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                      </div>
-                      <h4>
-                        Support Team
-                        <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                      </h4>
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="footer"><a href="#">See All Messages</a></li>
-            </ul>
-          </li>
-          <li class="dropdown notifications-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-bell-o"></i>
-              <span class="label label-warning">10</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
-              <li>
-                <ul class="menu">
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-user text-red"></i> You changed your username
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="footer"><a href="#">View all</a></li>
-            </ul>
-          </li>
-          <li class="dropdown tasks-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-flag-o"></i>
-              <span class="label label-danger">9</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 9 tasks</li>
-              <li>
-                <ul class="menu">
-                  <li>
-                    <a href="#">
-                      <h3>
-                        Design some buttons
-                        <small class="pull-right">20%</small>
-                      </h3>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
-                             aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          <span class="sr-only">20% Complete</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="footer">
-                <a href="#">View all tasks</a>
-              </li>
-            </ul>
-          </li>
-          <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="{{-- {{ Auth::user()->path == null ? asset('img/default1.jpg') : asset(Auth::user()->path) }} --}}" class="user-image" alt="User Image">
-              <span class="hidden-xs">{{-- {{ Auth::user()->full_name }} --}}</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="user-header">
-                <img src="{{-- {{ Auth::user()->path == null ? asset('img/default1.jpg') : asset(Auth::user()->path) }} --}}" class="img-circle" alt="User Image">
-
-                <p>
-                  {{-- {{ Auth::user()->full_name }}
-                  <small>{{ Auth::user()->created_at }}</small> --}}
-                </p>
-              </li>
-              <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-primary">Profile</a>
-                </div>
-                <form action="{{-- {{ route('logout') }} --}}" class="pull-right" method="POST">
-                  {{-- {{csrf_field()}} --}}
-                  <button type="submit" class="btn btn-primary"  name="logout">
-                    {{__('Log out')}}
-                  </button>
-                </form>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+        <li class="dropdown user user-menu">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="{{asset('images/user.jpg')}}" class="user-image" alt="User Image">
+            <span class="hidden-xs">{{ Auth::user()->name }}</span>
+          </a>
+          <ul class="dropdown-menu">
+            <li class="user-header">
+              <p>
+                {{ Auth::user()->name }}
+                <small>{{ ('DateCreation: ')}}{{ Auth::user()->created_at }}</small>
+              </p>
+            </li>
+            <li class="user-footer">
+              <form action="{{ route('logout') }}" class="text-center" method="POST">
+                {{csrf_field()}}
+                <button type="submit" class="btn btn-primary"  name="logout">
+                  {{__('Log out')}}
+                </button>
+              </form>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,17 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::group(['namespace' => 'Backend', 'prefix' => 'nhomkinhadmin'],function() {
+Route::group(['namespace' => 'Backend', 'prefix' => 'nhomkinhadmin', 'middleware' => 'auth'],function() {
 	Route::get('/home', 'HomeController@index')->name('admin.index');
 	Route::resource('/users', 'UserController');
     Route::resource('/categories', 'CategoryController');
     Route::resource('news', 'NewsController');
 });
+
+Auth::routes();
