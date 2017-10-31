bf164fd7dd91a20a5239b30cad56353c15fbb12e
vhhieu95 <hieupr9695@gmail.com>
2017-10-31 15:16:11 +0700

    contact

---
+++ resources/views/backend/contacts/index.blade.php
@@ -0,0 +1,75 @@
+@extends('backend.layouts.main')
+@section('title','Quản lý liên hệ')
+@section('content')
+  <div class="content-wrapper">
+    <section class="content-header">
+      <h1>{{__('Quản lý liên hệ ')}}
+      </h1>
+      <ol class="breadcrumb">
+        <li>
+          <a href="#"><i class="fa fa-dashboard"></i>{{__('Home')}}</a>
+        </li>
+        <li class="active">{{__('Liên Hệ')}}</li>
+      </ol>
+    </section>
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <div class="title-news mb-10">
+                <h3 class="box-title title-header">{{__('Danh sách liên hệ ')}}</h3>
+              </div>
+            </div>
+            <div class="box-body">
+              @include('flash::message')
+              @include('backend.layouts.partials.modal')
+              <table id="data-table" class="table table-bordered table-striped table-hover">
+                <thead>
+                <tr>
+                  <th>{{__('Id')}}</th>
+                  <th>{{__('Tên')}}</th>
+                  <th>{{__('Email')}}</th>
+                  <th>{{__('Tiêu đề ')}}</th>
+                  <th class=" col-md-1 text-center">{{__('Option')}}</th>
+                </tr>
+                </thead>
+                <tbody>
+                  @foreach($contacts as $item)
+                  <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->email }}</td>
+                    <td>{{ $item->subject }}</td>
+                    <td class="text-center col-md-2">
+                      <div class="btn-option text-center">
+                        <a href="{{ route('contacts.show',$item->id) }}" class="btn fa fa-search news-btn pull-left btn-custom-option" data-original-title="Chi Tiết" data-toggle="tooltip">
+                        </a>
+                        <form action="{{ route('contacts.destroy',$item->id) }}" 
+                          method="POST" class="inline">
+                          {{csrf_field()}}
+                          {{method_field('DELETE')}}
+                          <button type="submit" class="news-btn fa fa-trash-o 
+                            btn-delete-item pull-left btn-custom-option"  
+                            data-original-title="Delete" data-toggle="tooltip"
+                            data-title="{{ __('Xác nhận xóa!') }}"
+                            data-confirm="{{ __('Bạn có chắc chắn xóa?') }}">
+                          </button>
+                        </form>
+                      </div>
+                    </td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+              <div class="cls-searchnews-not-found text-center" hidden="">
+                {{__('Không có dữ liệu')}}
+              </div>
+              {{ $contacts->render() }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+@endsection
---
+++ resources/views/backend/contacts/show.blade.php
@@ -0,0 +1,58 @@
+@extends('backend.layouts.main')
+
+@section('title', __('Quản lý liên hệ'))
+
+@section('content')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12  ">
+          <div class="box clearfix">
+            <div class="box box-primary">
+              <div class="box-header with-border">
+                <h3 class="box-title">{{ __('Liên Hệ ') }}</h3>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body no-padding">
+                <div class="mailbox-read-info">
+                  <h3>{{ __('Tên:') }} {{ $contact->name }}</h3>
+                  <h4>{{ __('Email:') }} {{ $contact->email }}
+                  <h4>{{ __('Số điện thoại:') }} {{ $contact->phone }}
+                    <span class="mailbox-read-time pull-right">{{ $contact->created_at }}</span>
+                  </h4>
+                </div>
+                <div class="">
+                    <h3>{{ $contact->subject }}</h3>
+                </div>
+                <div class="mailbox-read-message">
+                  <p>{{ $contact->content }} </p>
+                </div>
+              </div>
+              <div class="box-footer">
+                @include('backend.layouts.partials.modal')
+                <div class="pull-right">
+                  <form  class="form-delete" method="post" 
+                    action="{{ route('contacts.destroy', $contact->id) }}">
+                    {!! csrf_field() !!}
+                    {{ method_field('DELETE') }}
+                    <button class=" btn btn-default btn-delete-item fa fa-trash-o"
+                      data-title="{{ __('Xác nhận xóa!') }}"
+                      data-confirm="{{ __('Bạn có chắc chắn xóa?') }}" 
+                      type="submit" >
+                    </button>  
+                  </form> 
+                </div>
+                <a class="btn btn-default btn-custom" id="btn-go-back" 
+                  href="javascript:history.back()">
+                  <i class="fa fa-arrow-left"></i>{{ __(' Trở về') }}
+                </a>
+              </div>
+            </div>
+          </div>   
+        </div>   
+      </div>
+    </section>
+  </div>
+@endsection
