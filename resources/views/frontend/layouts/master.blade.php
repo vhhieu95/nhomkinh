f526901c5efef7487271d7b9c0ed1695651ff4b5
vhhieu95 <hieupr9695@gmail.com>
2017-11-03 16:26:01 +0700

    add favicon

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="{{ asset('frontend/images/favicon.png') }}">
 	<meta name="description" content="">
 	<meta name="author" content="">
 
